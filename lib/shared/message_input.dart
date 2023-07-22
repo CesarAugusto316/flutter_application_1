0da6cab05144382f18f3fb25f94af2e129c5cf62
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-07-22 14:00:53 -0500

    ✨ focus on inputText

--- lib/shared/message_input.dart
+++ lib/shared/message_input.dart
@@ -4,23 +4,42 @@ class MessageInput extends StatelessWidget {
   MessageInput({super.key});
 
   final inputController = TextEditingController();
+  final focusNode = FocusNode();
+
+  _outlineBorder(BuildContext context) => OutlineInputBorder(
+      borderRadius: BorderRadius.circular(50),
+      borderSide: BorderSide(color: Theme.of(context).colorScheme.primary));
+
+  _inputDecoration(BuildContext context) => InputDecoration(
+      filled: true,
+      hintText: 'End your message with ?',
+      enabledBorder: _outlineBorder(context),
+      focusedBorder: _outlineBorder(context),
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          print(inputController.value.text);
+          inputController.clear();
+          focusNode.unfocus();
+        },
+      ));
 
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.only(bottom: 20),
         child: TextFormField(
+          focusNode: focusNode,
           controller: inputController,
+          onTapOutside: (e) {
+            focusNode.unfocus();
+          },
           onFieldSubmitted: (value) {
             inputController.clear();
             print(value);
+            focusNode.requestFocus();
           },
-          decoration: const InputDecoration(
-              filled: true,
-              suffixIcon: IconButton(
-                icon: Icon(Icons.send_outlined),
-                onPressed: null,
-              )),
+          decoration: _inputDecoration(context),
         ));
   }
 }
